601e66494e7f15a881c9c4b3e25b0360f3566c65
wseungjin <bose1021@naver.com>
2020-07-09 00:07:20 +0900

    solved : 프로그래머스 해쉬 연습 3번

---
+++ programmars/hash/better3.py
@@ -0,0 +1,30 @@
+from itertools import combinations
+
+def solution(clothes):
+    
+    answer = 0
+    clothes = sorted(clothes,key= lambda x:x[1])
+    arrayNum = [1]
+    arrayName = clothes[0][1]
+    t=0
+    for i in range(1,len(clothes)):
+        if clothes[i][1]==arrayName:
+            arrayNum[t]=arrayNum[t]+1
+        else:
+            arrayName = clothes[i][1]
+            arrayNum.append(1)
+            t=t+1
+    
+    answer = arrayNum[0]+1
+    for i in range(1,len(arrayNum)):
+        answer = answer * (arrayNum[i]+1)
+    
+    answer = answer - 1
+    return answer        
+
+def main():
+    clothes = [["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]
+    
+    print(solution(clothes))
+    
+main()
